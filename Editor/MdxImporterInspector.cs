d4a65d2b7f78aee05477fe89a0770192dea12d2f
Adri <Adri.Ciborro.Montes@gmail.com>
2021-03-15 21:28:07 +0100

    Minor changes

--- Editor/MdxImporterInspector.cs
+++ Editor/MdxImporterInspector.cs
@@ -6,11 +6,13 @@ public class MdxImporterInspector : ScriptedImporterEditor
 {
     private SerializedProperty importMaterials;
     private SerializedProperty importAnimations;
+    private SerializedProperty frameRate;
 
     public override void OnEnable()
     {
         importMaterials = serializedObject.FindProperty("importMaterials");
         importAnimations = serializedObject.FindProperty("importAnimations");
+        frameRate = serializedObject.FindProperty("frameRate");
         base.OnEnable();
     }
 
@@ -19,6 +21,7 @@ public class MdxImporterInspector : ScriptedImporterEditor
         serializedObject.Update();
         importMaterials.boolValue = EditorGUILayout.Toggle("Import Materials", importMaterials.boolValue);
         importAnimations.boolValue = EditorGUILayout.Toggle("Import Animations", importAnimations.boolValue);
+        frameRate.floatValue = EditorGUILayout.Slider("Frame Rate", frameRate.floatValue, 480, 1920);
         serializedObject.ApplyModifiedProperties();
         ApplyRevertGUI();
     }
