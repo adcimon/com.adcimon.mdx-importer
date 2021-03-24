b14df6b3ffee97529b9739b0af47ff474ba5dffc
adcimon <37084114+adcimon@users.noreply.github.com>
2021-03-24 20:51:39 +0100

    Added import tangents toggle

--- Editor/MdxScriptedImporterInspector.cs
+++ Editor/MdxScriptedImporterInspector.cs
@@ -1,17 +1,19 @@
 ﻿using UnityEditor;
-
+using UnityEditor.AssetImporters;
 
 [CustomEditor(typeof(MdxScriptedImporter))]
-public class MdxScriptedImporterInspector : UnityEditor.AssetImporters.ScriptedImporterEditor
+public class MdxScriptedImporterInspector : ScriptedImporterEditor
 {
     private SerializedProperty importMaterials;
     private SerializedProperty importAnimations;
+    private SerializedProperty importTangents;
     private SerializedProperty frameRate;
 
     public override void OnEnable()
     {
         importMaterials = serializedObject.FindProperty("importMaterials");
         importAnimations = serializedObject.FindProperty("importAnimations");
+        importTangents = serializedObject.FindProperty("importTangents");
         frameRate = serializedObject.FindProperty("frameRate");
         base.OnEnable();
     }
@@ -21,6 +23,7 @@ public class MdxScriptedImporterInspector : UnityEditor.AssetImporters.ScriptedI
         serializedObject.Update();
         importMaterials.boolValue = EditorGUILayout.Toggle("Import Materials", importMaterials.boolValue);
         importAnimations.boolValue = EditorGUILayout.Toggle("Import Animations", importAnimations.boolValue);
+        importTangents.boolValue = EditorGUILayout.Toggle("Import Tangents", importTangents.boolValue);
         frameRate.floatValue = EditorGUILayout.Slider("Frame Rate", frameRate.floatValue, 480, 1920);
         serializedObject.ApplyModifiedProperties();
         ApplyRevertGUI();
