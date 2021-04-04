2a4f8f299885682cd256994fe7fabdd12534386c
adcimon <37084114+adcimon@users.noreply.github.com>
2021-04-04 20:53:32 +0200

    Fixed translation animations bug

--- Editor/MdxScriptedImporterInspector.cs
+++ Editor/MdxScriptedImporterInspector.cs
@@ -1,30 +1,63 @@
-﻿using UnityEditor;
+﻿using UnityEngine;
+using UnityEditor;
 using UnityEditor.AssetImporters;
 
 [CustomEditor(typeof(MdxScriptedImporter))]
 public class MdxScriptedImporterInspector : ScriptedImporterEditor
 {
     private SerializedProperty importMaterials;
+    private SerializedProperty addMaterialsToAsset;
+
     private SerializedProperty importAnimations;
+    private SerializedProperty addAnimationsToAsset;
     private SerializedProperty importTangents;
     private SerializedProperty frameRate;
 
     public override void OnEnable()
     {
         importMaterials = serializedObject.FindProperty("importMaterials");
+        addMaterialsToAsset = serializedObject.FindProperty("addMaterialsToAsset");
+
         importAnimations = serializedObject.FindProperty("importAnimations");
+        addAnimationsToAsset = serializedObject.FindProperty("addAnimationsToAsset");
         importTangents = serializedObject.FindProperty("importTangents");
         frameRate = serializedObject.FindProperty("frameRate");
+
         base.OnEnable();
     }
 
     public override void OnInspectorGUI()
     {
         serializedObject.Update();
-        importMaterials.boolValue = EditorGUILayout.Toggle("Import Materials", importMaterials.boolValue);
-        importAnimations.boolValue = EditorGUILayout.Toggle("Import Animations", importAnimations.boolValue);
-        importTangents.boolValue = EditorGUILayout.Toggle("Import Tangents", importTangents.boolValue);
-        frameRate.floatValue = EditorGUILayout.Slider("Frame Rate", frameRate.floatValue, 480, 1920);
+
+        // Materials.
+        EditorGUILayout.BeginHorizontal();
+        {
+            importMaterials.boolValue = EditorGUILayout.Toggle(importMaterials.boolValue, GUILayout.Width(15));
+            GUILayout.Label("Materials");
+        }
+        EditorGUILayout.EndHorizontal();
+        if( importMaterials.boolValue )
+        {
+            addMaterialsToAsset.boolValue = EditorGUILayout.Toggle("Add Materials to Asset", addMaterialsToAsset.boolValue);
+        }
+
+        EditorGUILayout.Space(20);
+
+        // Animations.
+        EditorGUILayout.BeginHorizontal();
+        {
+            importAnimations.boolValue = EditorGUILayout.Toggle(importAnimations.boolValue, GUILayout.Width(15));
+            GUILayout.Label("Animations");
+        }
+        EditorGUILayout.EndHorizontal();
+        if( importAnimations.boolValue )
+        {
+            addAnimationsToAsset.boolValue = EditorGUILayout.Toggle("Add Animations to Asset", addAnimationsToAsset.boolValue);
+            importTangents.boolValue = EditorGUILayout.Toggle("Import Tangents", importTangents.boolValue);
+            frameRate.floatValue = EditorGUILayout.Slider("Frame Rate", frameRate.floatValue, 480, 1920);
+        }
+
         serializedObject.ApplyModifiedProperties();
         ApplyRevertGUI();
     }
--- Runtime/MdxExtension.cs
+++ Runtime/MdxExtension.cs
@@ -7,4 +7,14 @@ public static class MdxExtension
     {
         return new Vector3(cvector3.X, cvector3.Y, cvector3.Z);
     }
+
+    public static Vector3 SwapYZ( this Vector3 vector3 )
+    {
+        return new Vector3(vector3.x, vector3.z, vector3.y);
+    }
+
+    public static Quaternion ToQuaternion( this CVector4 cvector4 )
+    {
+        return new Quaternion(cvector4.X, cvector4.Y, cvector4.Z, cvector4.W);
+    }
 }
